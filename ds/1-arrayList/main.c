d98a9f9a35862354c8d7d3186551db669bca82c0
hanchao <1035710787@qq.com>
2020-08-12 20:01:04 +0800

    arraylist

--- ds/1-arrayList/main.c
+++ ds/1-arrayList/main.c
@@ -6,95 +6,72 @@
 //  Copyright © 2020 jianghao. All rights reserved.
 //
 
-
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-
-typedef struct{
-    int *head;
-    int lenth;
+typedef struct {
+    int* head;
+    int length;
     int capacity;
-}SeqList;
+} SeqList;
 
-//初始化顺序表
-SeqList* initSeqList(int cap){
-    
+// 初始化顺序表
+SeqList* initSeqList() {
     SeqList* pl = (SeqList*)malloc(sizeof(SeqList));
-    pl ->capacity =cap;
-    pl ->head=(int *)malloc(sizeof(int)*cap);
-    pl ->lenth=0;
+    pl->capacity = 10;
+    pl->head = (int *)malloc(sizeof(int) * 10);
+    pl->length = 0;
     return pl;
 }
 
-//增加数据
-void add(SeqList* pl, int value, int pos){
-    if (pos > pl->lenth || pos < 0) {
-        printf("插入位置有误\n");
+// 增加数据
+void add(SeqList* pl,int pos, int value) {
+    if (pl->length == pl->capacity) {
+        // 容量已满
+        pl->capacity <<= 1;
+        pl->head = (int *)realloc(pl->head, pl->capacity * sizeof(int));
     }
-    
-    for (int i = pl->lenth-1; i>=pos; i--) {
-        pl->head[i+1] = pl->head[i];
+
+    // 从length-1到pos的元素都往后复制一次
+    for (int i = pl->length; i > pos; i--) {
+        pl->head[i] = pl->head[i-1];
     }
+
     pl->head[pos] = value;
-    pl->lenth++;
-    
-    if (pl->lenth > pl->capacity) {
-        printf("容量已满\n");
-    }
-    
+    pl->length++;
 }
 
-//删除数据
-void del(SeqList* pl, int pos){
-    if (pos > pl->lenth-1 || pos < 0) {
-        printf("删除位置有误\n");
-    }
-    
-    for (int i=pos; i< pl->lenth-1; i++) {
-        pl->head[i] = pl->head[i+1];
-    }
-    pl->lenth--;
-    
-    if (pl ->lenth == 0) {
-        printf("已空,无法删除\n");
+// 删除数据
+void del(SeqList* pl, int pos) {
+    if (pos >= 0 && pos < pl->length) {
+        // 从pos到length-1的元素都往左移动一次
+        for (int i = pos; i < pl->length-1; i++) {
+            pl->head[i] = pl->head[i+1];
+        }
+        pl->length--;
+    } else {
+        printf("pos(%d)非法，当前length为%d\n", pos, pl->length);
     }
 }
 
-
-//打印数据
-void print(SeqList* pl){
-    for (int i=0; i<pl->lenth; i++) {
-        printf("%4d",pl->head[i]);
+void print(SeqList* pl) {
+    for (int i = 0; i < pl->length; i++) {
+        printf("%5d", pl->head[i]);
     }
     printf("\n");
 }
 
+int main() {
+    SeqList* pl = initSeqList();
+    for (int i = 0; i < 20; i++) {
+        add(pl, i, i * 100);
+        print(pl);
+    }
 
-int main(int argc, const char * argv[]) {
-    SeqList* pl=initSeqList(10);
-    /*
-    for (int i=0; i<10; i++) {
-        add(pl, i*100, i);
+    for (int i = 0; i < 20; i++) {
+        del(pl, pl->length-1);
+        print(pl);
     }
-    print(pl);
-    */
-    
-    /*
-    for (int i=0; i<10; i++) {
-        del(pl);
-    }
-    print(pl);
-    */
-     
-    
-    add(pl, 100, 0);
-    add(pl, 200 ,1);
-    print(pl);
-    
-    del(pl, 1);
-    print(pl);
-     
+
     return 0;
 }
